e53445530bf3c9561059d595d3cfd51be0a00e4c
GemmaYiQiZhou <gsm3cx@virginia.edu>
2025-04-21 23:24:07 -0400

    vampire attack, bullet collision, enemy death

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -5,4 +5,8 @@ var health: int = 100
 
 func handle_hit():
 	health -= 20
-	print("enemy hit", health)
+	if health <= 0:
+		$AnimatedSprite2D.animation = "dead"
+		$AnimatedSprite2D.speed_scale = 2.5
+		$AnimatedSprite2D.play()
+		queue_free()
