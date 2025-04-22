cb416f062509ea09586c7040cc1493a64f2d4875
gckang <76260739+gckang@users.noreply.github.com>
2025-04-22 17:31:50 -0400

    add dash

--- scripts/vampire.gd
+++ scripts/vampire.gd
@@ -9,6 +9,8 @@ signal player_fired_bullet(bullet, position, direction)
 @onready var attack_direction = $AnimatedSprite2D/AttackDirection
 @onready var attack_cooldown = $AttackCooldown
 
+const START_SPEED : int = 100
+const BOOST_SPEED : int = 200
 var health: int = 100
 
 func _process(_delta: float) -> void:
@@ -53,39 +55,9 @@ func handle_hit():
 	health -= 20
 	print("player hit", health)
 
-#Working code from Grace:
-#signal shoot
-#const SPEED = 300.0
-#const ACCEL = 2.0
-#var input: Vector2
+func boost():
+	$BoostTimer.start()
+	speed = BOOST_SPEED
 
-#func get_input():
-	#input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	#input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	#
-	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		#var dir = get_global_mouse_position() - position
-		#shoot.emit(position, dir)
-	#
-	#return input.normalized()
-	
-#func _physics_process(delta):
-#	var playerInput = get_input()
-#	velocity = lerp(velocity, playerInput * SPEED, delta * ACCEL)
-#	move_and_slide()
-	
-	# player direction except we need sprites for each thing so this also doesnt work :skull:
-	#var mouse = get_local_mouse_position()
-	#var angle = snappedf(mouse.angle(), PI/4) / (PI / 4)
-	#angle = wrapi(int(angle), 0, 8) 
-	#$AnimatedSprite2D.animation = "walk" + str(angle)
-	
-	# should limit it to only animate when walking but its not working :/
-	#if velocity.length() != 0:
-		#$AnimatedSprite2D.play()
-	#else:
-		#$AnimatedSprite2D.stop()
-		#$AnimatedSprite2D.frame = 3
-		
-
-	
+func _on_boost_timer_timeout() -> void:
+	speed = START_SPEED
