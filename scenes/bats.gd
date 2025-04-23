0e61d84cb7a674e83876604633e000f76fbb1a12
Brian Xiao <brianjxiao@gmail.com>
2025-04-23 00:28:50 -0400

    implemented bat attack functionality

---
+++ scenes/bats.gd
@@ -0,0 +1,33 @@
+extends Area2D
+class_name  bats
+
+@export var speed: float = 100
+@onready var kill_timer = $KillTimer
+
+var direction: Vector2 = Vector2.ZERO
+var ignore_body: Node2D = null  # ← Who fired the bullet
+
+
+func _ready() -> void:
+	kill_timer.start()
+	$AnimatedSprite2D.play("bats")
+
+func _physics_process(delta: float) -> void:
+	if direction != Vector2.ZERO:
+		position += direction.normalized() * speed * delta
+	
+
+func set_direction(dir : Vector2) -> void:
+	direction = dir
+	rotation += direction.angle()
+
+
+func _on_kill_timer_timeout() -> void:
+	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == ignore_body:
+		return
+	if body.has_method("handle_hit"):
+		body.handle_hit()
+		queue_free()
--- scripts/vampire.gd
+++ scripts/vampire.gd
@@ -11,10 +11,23 @@ signal player_fired_bullet(bullet, position, direction)
 @onready var attack_direction = $AnimatedSprite2D/AttackDirection
 @onready var attack_cooldown = $AttackCooldown
 
+@onready var bats_cooldown = $BatsCooldown
+
 const START_SPEED : int = 100
 const BOOST_SPEED : int = 200
 var health: int = 100
 var bats_active = false
+
+func _ready():
+	var direction = Vector2.RIGHT
+	var bats_instance = Bats.instantiate()
+	bats_instance.ignore_body = self
+	bats_instance.set_direction(direction)
+	bats_instance.get_node("AnimatedSprite2D").play("bat")
+	bats_instance.get_node("AnimatedSprite2D").scale = Vector2(0.1, 0.1)
+	add_child(bats_instance)
+	bats_instance.global_position = attack_spawn_point.global_position
+
 
 func _process(_delta: float) -> void:
 	var movement_direction := Vector2.ZERO
@@ -45,11 +58,12 @@ func _unhandled_input(event: InputEvent) -> void:
 func activate_bats():
 	print("do we get here x2???")
 	bats_active = true
-	print(bats_active)
-	$BatsTimer.start()
+	print(bats_active )
 
 func _on_bats_timer_timeout() -> void:
-	bats_active = false
+	#bats_active = false
+	#right now this doesnt do anything
+	pass
 
 func shoot():
 	$AnimatedSprite2D.animation = "attack"
@@ -62,18 +76,22 @@ func shoot():
 		bullet_instance.ignore_body = self
 		var direction = (attack_direction.global_position - attack_spawn_point.global_position).normalized()
 		emit_signal("player_fired_bullet", bullet_instance, attack_spawn_point.global_position, direction)
-		
-		# Bats Bullet
-		print(bats_active)
-		if bats_active:
-			print("we are shoooting bats too")
-			var bats_instance = Bats.instantiate()
-			bats_instance.ignore_body = self
-			bats_instance.get_node("AnimatedSprite2D").play("bat")
-			$AnimatedSprite2D.scale = Vector2(1, 1)
-			emit_signal("player_fired_bullet", bats_instance, attack_spawn_point.global_position, direction)
-		
 		attack_cooldown.start()
+		
+	if bats_cooldown.is_stopped() and bats_active:
+		var bats_instance = Bats.instantiate()
+		var direction = (attack_direction.global_position - attack_spawn_point.global_position).normalized()
+		var angle_variation = deg_to_rad(randf_range(-15, 15))  # up to ±15 degrees
+		var varied_direction = direction.rotated(angle_variation)
+		bats_instance.set_direction(varied_direction)
+
+		bats_instance.ignore_body = self
+		bats_instance.get_node("AnimatedSprite2D").play("bat")
+		bats_instance.get_node("AnimatedSprite2D").scale = Vector2(1, 1) 
+		emit_signal("player_fired_bullet", bats_instance, attack_spawn_point.global_position, direction)
+		
+		bats_cooldown.start()
+		
 
 func handle_hit():
 	health -= 20
