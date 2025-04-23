13c5644da12f1ef2647f1def7ebbac80c6bddab7
gckang <76260739+gckang@users.noreply.github.com>
2025-04-23 14:13:37 -0400

    add all powerups in again

--- scripts/item.gd
+++ scripts/item.gd
@@ -5,7 +5,7 @@ var item_type : int 	# different types of power ups; 1 - bats, 2 - shadow, 3 - d
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# rn only have bats and dash bc shadow isn't setup yet
-	item_type = randi_range(0, 1)	# set to random powerup
+	item_type = randi_range(0, 2)	# set to random powerup
 	$AnimatedSprite2D.play()
 	print("Item Ready!")
 
@@ -21,12 +21,12 @@ func _on_body_entered(body: Node2D) -> void:
 		if body.has_method("activate_bats"):
 			print("do we get here???")
 			body.activate_bats()
-	elif item_type == 2:
+	elif item_type == 1:
 		print("Shadow")
 		if body.has_method("activate_shadow"):
 			body.activate_shadow()
 	#elif item_type == 2:
-	elif item_type == 1:
+	elif item_type == 2:
 		print("Dash")
 		#body.boost()
 		if body.has_method("activate_dash"):
