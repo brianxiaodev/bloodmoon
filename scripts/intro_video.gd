e75339b97671e72b8d7738b0f601a6b0b7db463e
gckang <76260739+gckang@users.noreply.github.com>
2025-04-28 07:05:18 -0400

    working intro video, however label to skip is not showing

--- scripts/intro_video.gd
+++ scripts/intro_video.gd
@@ -1,12 +1,29 @@
 extends VideoStreamPlayer
 
+@onready var viewport_size = Vector2(get_viewport().size)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if stream:
+		await self.ready
+		var texture = get_video_texture()
+		var video_size = texture.get_size()
+		_fit_video_to_screen(video_size)
 
-
-
+func _fit_video_to_screen(video_size: Vector2) -> void:
+	var video_aspect = video_size.x / video_size.y
+	var viewport_aspect = viewport_size.x / viewport_size.y
+	if video_aspect > viewport_aspect:
+		# Video is wider than screen -> fit width
+		scale.x = viewport_size.x / video_size.x
+		scale.y = scale.x
+	else:
+		# Video is taller -> fit height
+		scale.y = viewport_size.y / video_size.y
+		scale.x = scale.y
+		
+	# Center the video
+	position = (viewport_size - video_size * scale) / 2
 
 
 func _on_finished() -> void:
